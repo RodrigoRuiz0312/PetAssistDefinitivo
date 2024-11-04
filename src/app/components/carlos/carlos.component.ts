b0fa336d67f1522ab53fd3c1359a4d9caadc99bd
RodrigoRuiz0312 <21690098@tecvalles.mx>
2024-11-04 13:55:11 -0600

    nuevo componente

---
+++ src/app/components/carlos/carlos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carlos',
+  standalone: true,
+  imports: [],
+  templateUrl: './carlos.component.html',
+  styleUrl: './carlos.component.css'
+})
+export class CarlosComponent {
+
+}
